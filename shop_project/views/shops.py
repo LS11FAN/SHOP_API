cad5f6f44e3b8b9353480f8dc36a9139e12ac247
LS11FAN <poz.2014@mail.ru>
2023-11-21 18:54:12 +0300

    add login and register, fir reviews, some bug fixes

--- shop_project/views/shops.py
+++ shop_project/views/shops.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from shop_project.models import Category, Product, Review
 from shop_project.forms import ReviewForm
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
 
+
+@login_required
 def product_list(request, category_slug=None):
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
@@ -19,6 +25,7 @@ def product_list(request, category_slug=None):
     return render(request, "list.html", context)
 
 
+@login_required
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     reviews = Review.objects.filter(product=product)
@@ -31,10 +38,22 @@ def product_detail(request, id, slug):
             review.product = product
             review.user = request.user
             review.save()
-            return redirect('products:product_detail', id=id, slug=slug)
+            return redirect('product_detail', id=product.id, slug=product.slug)
     context = {
         'product': product,
         'reviews': reviews,
         'form': form,
     }
     return render(request, "detail.html", context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('shop_project:product_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
